24dff3d328c0b674cbc05a2be2577bbe707009b7
Jo Wagner <jowagner@limago.de>
2026-09-22 10:28:58 +0200

    initial commit

--- WebApp/src/main/java/de/atruvia/webapp/presentation/controller/v1/PersonenController.java
+++ WebApp/src/main/java/de/atruvia/webapp/presentation/controller/v1/PersonenController.java
@@ -1,14 +1,18 @@
 package de.atruvia.webapp.presentation.controller.v1;
 
 import de.atruvia.webapp.presentation.dto.PersonDto;
+import de.atruvia.webapp.presentation.error.IdMismatchException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.UUID;
@@ -88,4 +92,29 @@ public class PersonenController {
         return ResponseEntity.ok(list);
 
     }
+
+    @DeleteMapping(path = "/{id}" )
+    public ResponseEntity<Void> deletePerson(@PathVariable  UUID id) {
+        if(id.toString().endsWith("1"))
+            return ResponseEntity.notFound().build();
+
+        System.out.println("Person wurde geloescht");
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(path="",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> einfuegen(@Valid @RequestBody PersonDto personDto, UriComponentsBuilder uriBuilder) {
+
+        System.out.println("Person wurde gespeichert");
+        UriComponents uriComponents = uriBuilder.path("/v1/personen/{id}").buildAndExpand(personDto.getId());
+        return ResponseEntity.created(uriComponents.toUri()).build();
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> update(@PathVariable UUID id,@Valid @RequestBody PersonDto personDto){
+        if (! id.equals(personDto.getId())) throw new IdMismatchException("ID mismatch");
+        System.out.println("Person geaendert");
+        return ResponseEntity.ok().build();
+    }
+
 }
---
+++ WebApp/src/main/java/de/atruvia/webapp/presentation/error/IdMismatchException.java
@@ -0,0 +1,23 @@
+package de.atruvia.webapp.presentation.error;
+
+public class IdMismatchException extends RuntimeException{
+
+    public IdMismatchException() {
+    }
+
+    public IdMismatchException(final String message) {
+        super(message);
+    }
+
+    public IdMismatchException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public IdMismatchException(final Throwable cause) {
+        super(cause);
+    }
+
+    public IdMismatchException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
